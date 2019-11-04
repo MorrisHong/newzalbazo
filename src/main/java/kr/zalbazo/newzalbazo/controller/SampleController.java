da1bb08e55e989ef91dc4c6a89cb15309d0c74a1
gracelove <govlmo91@gmail.com>
2019-11-04 16:07:11 +0900

    init

--- src/main/java/kr/zalbazo/newzalbazo/controller/SampleController.java
+++ src/main/java/kr/zalbazo/newzalbazo/controller/SampleController.java
@@ -1,7 +1,20 @@
 package kr.zalbazo.newzalbazo.controller;
 
+import kr.zalbazo.newzalbazo.service.SampleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
+
+    @Autowired
+    private SampleService sampleService;
+
+    @GetMapping("/hi")
+    @ResponseBody
+    public String hi() {
+        return "hi " +sampleService.getName() + " : " + sampleService.now();
+    }
 }
--- src/main/java/kr/zalbazo/newzalbazo/service/SampleService.java
+++ src/main/java/kr/zalbazo/newzalbazo/service/SampleService.java
@@ -1,7 +1,21 @@
 package kr.zalbazo.newzalbazo.service;
 
+import kr.zalbazo.newzalbazo.mapper.TimeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SampleService {
+
+
+    @Autowired
+    private TimeMapper timeMapper;
+
+    public String getName() {
+        return "grace";
+    }
+
+    public String now() {
+        return timeMapper.getTime();
+    }
 }
